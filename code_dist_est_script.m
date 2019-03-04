c5a670b25a5901d324959c4d07319b5b219c1d6a
USA_VS <L@lcrypto.com>
2019-03-04 13:28:19 +0300

    Update code_dist_est_script.m

--- code_dist_est_script.m
+++ code_dist_est_script.m
@@ -1,17 +1,22 @@
-folder_path = 'D:\BG2_proposal\Samsung\'
-files = dir(folder_path);
-files = files(3:length(files));
-len = length(files);
+% folder_path = 'C:\Users\u00288620\Desktop\art\samsung\Number Geometry\BG2_Number_Geometry_Code_Distance-master\our\'
+% files = dir(folder_path);
+% files = files(3:length(files));
+% len = length(files);
 
-for i = 1:len
-    filename = strcat(folder_path, files(i).name);
+%for i = 1:len
+ %   filename = strcat(folder_path, files(i).name);
 
 
 
-    H=qc2sparse('a=2_1_3_64.txt');
+   % H=qc2sparse('a2_2_1_3.txt');
+    %H=qc2sparse('a2_4_1_3.txt');
+    H=qc2sparse('a2_8_1_3.txt');
+  % H=qc2sparse(filename);
      [h, g] = h2g(H,2) ;
     %sparse(mod(g*h',2))
-    fileID = fopen('generator_a2_1_3_64.txt','w');
+    %filename_gen=[filename, '_generator.txt'];
+    %fileID = fopen(filename_gen,'w');
+    fileID = fopen('a2_2_1_3.txt.gen','w');
     fprintf(fileID,'%d %d 2 \n',size(g,1),size(g,2));
     for i = 1:size(g,1)
         for j = 1:size(g,2)
@@ -19,12 +24,17 @@ for i = 1:len
         end
         fprintf(fileID,'\n');
     end
-]
-    fclose(fileID);
-    fileID = fopen('samsung_2_1_3_64.bat','w'); 
-	 %fprintf(fileID,'%s', 'codedist_HouseOrtg_1_core.exe < generator_a2_1_3_64.txt');
-     fprintf(fileID,'%s', 'codedist_Prob_Fuka_Kashi_GPU.exe < generator_a2_1_3_64.txt');
-     fclose(fileID);
-     !samsung_2_1_3_64.bat
 
-end
+    fclose(fileID);
+   % filename_gen_bat=[filename_gen, '.bat'];
+    %fileID = fopen(filename_gen_bat,'w'); 
+    fileID = fopen('a2_2_1_3.txt.gen.bat','w'); 
+	% fprintf(fileID,'%s', 'codedist_HouseOrtg_1_core.exe < ', filename_gen);
+    fprintf(fileID,'%s', 'codedist_HouseOrtg_1_core.exe < ',  'a2_2_1_3.txt.gen');
+    
+    % fprintf(fileID,'%s', 'codedist_Prob_Fuka_Kashi_GPU.exe < generator_a2_1_3_64.txt');
+     fclose(fileID);
+    % system(filename_gen_bat)
+     !a2_2_1_3.txt.gen.bat
+
+%end
